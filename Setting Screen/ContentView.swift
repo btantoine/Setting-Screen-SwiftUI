146f9bfc3acb861b71f0a12c0e9249c3ba62a004
Dhanraj Chavan <dc6707914@gmail.com>
2020-04-18 15:46:47 +0530

    Initial Commit

---
+++ Setting Screen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Setting Screen
+//
+//  Created by Dhanraj Sudhir Chavan on 18/04/20.
+//  Copyright © 2020 Dhanraj Chavan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
